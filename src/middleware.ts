4636134239a804053ddaafad7ac07a4fae61beed
Swalah Codes <ahmadsalahuddeen6017@gmail.com>
2024-03-16 11:03:22 +0530

    fixed middlware file

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,42 +2,50 @@ import { authMiddleware } from "@clerk/nextjs";
 import { NextURL } from "next/dist/server/web/next-url";
 import { NextResponse } from "next/server";
 
- 
 export default authMiddleware({
-  publicRoutes: ["/site", "/uploadthing"],
-async beforeAuth(auth , req){},
-async afterAuth(auth , req){
-//rewrite for domains
-  const url = req.nextUrl
-const searchParams = url.searchParams.toString()
-const hostname = req.headers
- 
-const pathWtihSearchParams = `${url.pathname}${searchParams.length > 0 ? `?${searchParams}`
-: ""}`
+  publicRoutes: ['/site', '/api/uploadthing'],
+  async beforeAuth(auth, req) {},
+  async afterAuth(auth, req) {
+    //rewrite for domains
+    const url = req.nextUrl
+    const searchParams = url.searchParams.toString()
+    let hostname = req.headers
 
-// if subdomain exists
+    const pathWithSearchParams = `${url.pathname}${
+      searchParams.length > 0 ? `?${searchParams}` : ''
+    }`
 
-const customSubDomain = hostname.get('host')?.split(`${process.env.NEXT_PUBLIC_DOMAIN}`).filter(Boolean)[0]
+    //if subdomain exists
+    const customSubDomain = hostname
+      .get('host')
+      ?.split(`${process.env.NEXT_PUBLIC_DOMAIN}`)
+      .filter(Boolean)[0]
 
-if(customSubDomain){
-  return NextResponse.rewrite(new URL(`${customSubDomain}${pathWtihSearchParams}`))
-}
+    if (customSubDomain) {
+      return NextResponse.rewrite(
+        new URL(`/${customSubDomain}${pathWithSearchParams}`, req.url)
+      )
+    }
 
-if(url.pathname === '/sign-in' || url.pathname === '/sign-up'){
-  return NextResponse.redirect((new URL('/agency/sign-in', req.url)))
-}
+    if (url.pathname === '/sign-in' || url.pathname === '/sign-up') {
+      return NextResponse.redirect(new URL(`/agency/sign-in`, req.url))
+    }
 
-  if(url.pathname === '/' ||
-   (url.pathname === '/site' && url.host === process.env.NEXT_PUBLIC_DOMAIN)
-    ){
+    if (
+      url.pathname === '/' ||
+      (url.pathname === '/site' && url.host === process.env.NEXT_PUBLIC_DOMAIN)
+    ) {
       return NextResponse.rewrite(new URL('/site', req.url))
-}
-if(url.pathname.startsWith('/agency') || url.pathname.startsWith('/subaccount')){
-return NextResponse.rewrite(new URL(`${pathWtihSearchParams}`, req.url))
-}
+    }
 
-},
-});
+    if (
+      url.pathname.startsWith('/agency') ||
+      url.pathname.startsWith('/subaccount')
+    ) {
+      return NextResponse.rewrite(new URL(`${pathWithSearchParams}`, req.url))
+    }
+  },
+})
  
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
