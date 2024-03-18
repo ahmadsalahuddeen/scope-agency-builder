921346345c474a930ef6edd0364edfd762963632
Swalah Codes <ahmadsalahuddeen6017@gmail.com>
2024-03-18 14:51:16 +0530

    agency deails , file uplaod comp

--- src/components/forms/agency-details.tsx
+++ src/components/forms/agency-details.tsx
@@ -1,9 +1,11 @@
 'use client';
 import { Agency } from '@prisma/client';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useToast } from '../ui/use-toast';
 import { useRouter } from 'next/navigation';
 import { AlertDialog } from '@radix-ui/react-alert-dialog';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
 import {
   Card,
   CardContent,
@@ -11,24 +13,56 @@ import {
   CardHeader,
   CardTitle,
 } from '../ui/card';
-import { Form } from '../ui/form';
-import { useForm } from 'react-hook-form';
-import * as z from 'zod'
+import { Form, FormControl, FormField, FormItem, FormLabel } from '../ui/form';
+import * as z from 'zod';
+import FileUpload from '../global/file-upload';
 
 type Props = {
   data?: Partial<Agency>;
 };
 
 const FormSchema = z.object({
-  name: z.string().min(1, {message: 'Agency name must be atleast 2 chars.'})
-})
+  name: z.string().min(1, { message: 'Agency name must be atleast 2 chars.' }),
+  agencyLogo: z.string().min(1),
+  companyEmail: z.string().min(1),
+  companyPhone: z.string().min(1),
+  whiteLabel: z.boolean(),
+  address: z.string().min(1),
+  city: z.string().min(1),
+  zipCode: z.string().min(1),
+  state: z.string().min(1),
+  country: z.string().min(1),
+});
 
-const AgencyDetailsComp = (props: Props) => {
+const AgencyDetailsComp = ({ data }: Props) => {
   const { toast } = useToast();
   const router = useRouter();
   const [deletingAgency, setDeletingAgency] = useState(false);
 
-  const form  = useForm<z.infer<typeof FormSchema>>()
+  const form = useForm<z.infer<typeof FormSchema>>({
+    mode: 'onChange',
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      name: data?.name,
+      agencyLogo: data?.agencyLogo,
+      companyEmail: data?.companyEmail,
+      companyPhone: data?.companyPhone,
+      whiteLabel: data?.whiteLabel || false,
+      address: data?.address,
+      city: data?.city,
+      zipCode: data?.zipCode,
+      state: data?.state,
+      country: data?.country,
+    },
+  });
+  const isLoading = form.formState.isLoading;
+  useEffect(() => {
+    if (data) {
+      form.reset(data);
+    }
+  }, [data]);
+
+  const handleSubmit = async () => {};
 
   return (
     <AlertDialog>
@@ -41,7 +75,25 @@ const AgencyDetailsComp = (props: Props) => {
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <Form></Form>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(handleSubmit)}
+              className="space-y-4"
+            >
+              <FormField
+                disabled={isLoading}
+                control={form.control}
+                name="agencyLogo"
+                render={({ field }) => 
+                <FormItem>
+                  <FormLabel>Agencylogo</FormLabel>
+                  <FormControl>
+                    <FileUpload ></FileUpload>
+                  </FormControl>
+                </FormItem>}
+              ></FormField>
+            </form>
+          </Form>
         </CardContent>
       </Card>
     </AlertDialog>
