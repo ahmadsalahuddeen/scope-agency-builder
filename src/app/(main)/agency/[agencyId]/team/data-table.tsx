420d193c907579bffec6aa625e99c9a1f9f5d1e4
Swalah Codes <ahmadsalahuddeen6017@gmail.com>
2024-03-30 14:20:31 +0530

    Team Page: update

--- src/app/(main)/agency/[agencyId]/team/data-table.tsx
+++ src/app/(main)/agency/[agencyId]/team/data-table.tsx
@@ -1,8 +1,10 @@
+'use client';
 import React from 'react';
 import {
   ColumnDef,
   flexRender,
   getCoreRowModel,
+  getFilteredRowModel,
   useReactTable,
 } from '@tanstack/react-table';
 
@@ -14,11 +16,14 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/table';
+import { useModal } from '@/providers/modal-provider';
+import { Search } from 'lucide-react';
+import { Input } from '@/components/ui/input';
 
 type Props = {};
 
 interface DataTableProps<TData, TValue> {
-  columns: ColumnDef<TData, TValue>;
+  columns: ColumnDef<TData, TValue>[];
   data: TData[];
   filterValue: string;
   actionButtonText?: React.ReactNode;
@@ -31,4 +36,30 @@ export default function DataTable<TData, TValue>({
   filterValue,
   actionButtonText,
   modalChildren,
-}: DataTableProps<TData, TValue>) {}
+}: DataTableProps<TData, TValue>) {
+
+  
+  const { setOpen } = useModal();
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+  });
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <div className=" flex items-center gap-2 py-4">
+          <Search />
+          <Input
+            placeholder="Search Name ..."
+            value={
+              (table.getColumn(filterValue)?.getFilterValue() as string) ?? ''
+            }
+          />
+        </div>
+      </div>
+    </>
+  );
+}
