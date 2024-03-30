ca3762e90e1c54b5f86c5679f299bfbe67d54d4b
Swalah Codes <ahmadsalahuddeen6017@gmail.com>
2024-03-30 06:53:53 +0530

    data table update

---
+++ src/app/(main)/agency/[agencyId]/team/data-table.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from '@tanstack/react-table';
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+
+type Props = {};
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>;
+  data: TData[];
+  filterValue: string;
+  actionButtonText?: React.ReactNode;
+  modalChildren?: React.ReactNode;
+}
+
+export default function DataTable<TData, TValue>({
+  columns,
+  data,
+  filterValue,
+  actionButtonText,
+  modalChildren,
+}: DataTableProps<TData, TValue>) {}
--- src/app/(main)/agency/[agencyId]/team/page.tsx
+++ src/app/(main)/agency/[agencyId]/team/page.tsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import { db } from '@/lib/db';
+import { currentUser } from '@clerk/nextjs';
+import React from 'react';
+import DataTable from './data-table';
 
-type Props = {}
+type Props = {
+  params: {
+    agencyId: string;
+  };
+};
 
-const Page = (props: Props) => {
-  return (
-    <div>Page</div>
-  )
-}
+const TeamPage = async ({ params }: Props) => {
+  const authUser = await currentUser();
+  const teamMembers = await db.user.findMany({
+    where: {
+      agencyId: params.agencyId,
+    },
+    include: {
+      Agency: {
+        include: {
+          SubAccount: true,
+        },
+      },
+      Permissions: true,
+    },
+  });
 
-export default Page
+  if (!authUser) return;
+
+  const agencyDetails = await db.agency.findUnique({
+    where: { id: params.agencyId },
+    include: { SubAccount: true },
+  });
+  if (!agencyDetails) return;
+
+
+  return <div>
+
+    <DataTable />
+  </div>;
+};
+
+export default TeamPage;
